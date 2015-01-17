81088d00bc57e933d631e11b829a22f1042b8e1c
ntoscano <Nicolas.Toscano@gmail.com>
2015-01-17 13:15:41 -0800

    combined my html with index html; added youtube.js(doesnt work yet) and deck.js(doesnt work yet)

---
+++ app/deck.js
@@ -0,0 +1,9 @@
+(function(){
+	var app = angular.module('get-deck', []);
+	app.controller('DeckController',['$http', function($http){
+		this.deck = {};
+		this.getdeck = function(deck){
+
+		};
+	}]);
+});
---
+++ app/youtube.js
@@ -0,0 +1,7 @@
+(function(){
+	var app angular.module('youtube-embed', ["get-deck"]);
+	app.controller('EmbedController', function(){
+		this.video = 
+
+	});
+});

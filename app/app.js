295fcbc533ce8b596b321764326ca34a2b8417a4
ntoscano <Nicolas.Toscano@gmail.com>
2015-01-17 14:48:24 -0800

    created get-deck module, still doesnt work

--- app/app.js
+++ app/app.js
@@ -3,9 +3,7 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'get-deck'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
--- app/deck.js
+++ app/deck.js
@@ -1,9 +1,13 @@
 (function(){
 	var app = angular.module('get-deck', []);
 	app.controller('DeckController',['$http', function($http){
+		var este = this
 		this.deck = {};
-		this.getdeck = function(deck){
-
-		};
+		$http.get('http://membright.com/api/v2/deck/149?format=json').success(function(data){
+			este.deck = data;
+			console.log("test");
+			console.log(data);
+		});
+		console.log("test2")
 	}]);
 });

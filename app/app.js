5ce64318728db4a38ce6a463bf73f9b513c527e1
ntoscano <Nicolas.Toscano@gmail.com>
2015-01-17 17:48:34 -0800

    cleaned up modules

--- app/app.js
+++ app/app.js
@@ -1,21 +1,9 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-var app =  angular.module('myApp', ['ngRoute'])
+var app =  angular.module('myApp', ["ngRoute","getDeck"])
 
 app.config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
 
-app.controller('DeckController',['$http', '$sce', function($http, $sce){
-    var este = this;
-    this.deck = {};
-	this.deck.videoUrl = "";
-	$http.get('http://membright.com/api/v2/deck/214?format=json').success(function(data){
-        este.deck = data;
-        console.log("test");
-        console.log(data);
-		este.deck.videoUrl = $sce.trustAsResourceUrl(este.deck.content.sourceUrl.replace("watch?v=","embed/"));
-	});
-	
-}]);
--- app/deck.js
+++ app/deck.js
@@ -1,13 +1,16 @@
 (function(){
-	var app = angular.module('get-deck', []);
-	app.controller('DeckController',['$http', function($http){
-		var este = this
-		this.deck = {};
-		$http.get('http://membright.com/api/v2/deck/149?format=json').success(function(data){
-			este.deck = data;
-			console.log("test");
-			console.log(data);
-		});
-		console.log("test2")
+	var app = angular.module('getDeck', []);
+
+
+	app.controller('DeckController',['$http', '$sce', function($http, $sce){
+    	var este = this;
+   		this.deck = {};
+   		this.deck.videoUrl = "";
+   		$http.get('http://membright.com/api/v2/deck/214?format=json').success(function(data){
+       		este.deck = data;
+       		console.log("test");
+       		console.log(data);
+       		este.deck.videoUrl = $sce.trustAsResourceUrl(este.deck.content.sourceUrl.replace("watch?v=","embed/"));
+    	});
 	}]);
-});
+})();

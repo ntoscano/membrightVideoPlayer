688a5695f61e2039562f2e4c9048cbfb1902c715
ntoscano <Nicolas.Toscano@gmail.com>
2015-01-17 17:26:11 -0800

    got the video embed working, tabs still dont work, modules are sloppy

--- app/app.js
+++ app/app.js
@@ -1,10 +1,21 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'get-deck'
-]).
-config(['$routeProvider', function($routeProvider) {
+var app =  angular.module('myApp', ['ngRoute'])
+
+app.config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
+
+app.controller('DeckController',['$http', '$sce', function($http, $sce){
+    var este = this;
+    this.deck = {};
+	this.deck.videoUrl = "";
+	$http.get('http://membright.com/api/v2/deck/214?format=json').success(function(data){
+        este.deck = data;
+        console.log("test");
+        console.log(data);
+		este.deck.videoUrl = $sce.trustAsResourceUrl(este.deck.content.sourceUrl.replace("watch?v=","embed/"));
+	});
+	
 }]);

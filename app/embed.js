fbf28ec532c5bc2ea3e8447cd1c0e2b90cf90c91
ntoscano <Nicolas.Toscano@gmail.com>
2015-01-22 12:19:17 -0800

    most current stable version, cards are still unclickable, videoId has issues

--- app/embed.js
+++ app/embed.js
@@ -1,6 +1,35 @@
 (function(){
 
 	var app = angular.module('app-directives', ['getDeck']);
+
+	app.directive('youtube', function($window){
+		return{
+			restrict: 'E',
+			scope: {
+				height: "@",
+				width: "@",
+				videoId: "@",
+			},
+			template: '<div></div>',
+			link: function(scope, element){
+				var tag = document.createElement('script');
+				tag.src = "https://www.youtube.com/iframe_api"
+				var firstScriptTag = document.getElementsByTagName('script')[0];
+				firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+				var player;
+
+				$window.onYouTubeIframeAPIReady = function(){
+					player = new YT.Player(element.children()[0], {
+						height: scope.height,
+						width: scope.width,
+						videoId: "Yocja_N5s1I" 
+					});
+				};
+			}
+		}
+	});
+
 	app.directive("embedLink", ['$sce', function($sce){
 		return{
 			restrict: 'A',
@@ -14,17 +43,7 @@
 					},
 					post : function (scope, iElement, iAttrs) {
 						console.log(scope.deck.name);
-						scope.videoEmbedUrl = $sce.trustAsResourceUrl(scope.deck.content.sourceUrl.replace("watch?v=","embed/"))
-						scope.cards = scope.deck.cards
-						for (var x in scope.deck.cards){
-                            scope.cards[x].question = scope.deck.cards[x].obj.question
-                            scope.cards[x].answer = scope.deck.cards[x].obj.answer
-                            console.log(scope.cards + "test");	
-							//scope.cards[x].onclick = function jumpTo(){
-							//	scope.videoEmbedUrl = $sce.trustAsResourceUrl(scope.cards[x].obj.links[0].url);
-							//	
-							//}
-						}
+						scope.videoEmbedUrl = $sce.trustAsResourceUrl(scope.deck.content.sourceUrl.replace("watch?v=","embed/"));
 					}
 				}
 			}
@@ -104,34 +123,7 @@
 		};
 	});
 
-	app.directive('youtube', function($window){
-		return{
-			restrict: 'E',
-			scope: {
-				height: "@",
-				width: "@",
-				videoId: "=",
-				deck: "="
-			},
-			template: '<div></div>',
-			link: function(scope, element){
-				var tag = document.createElement('script');
-				tag.src = "https://www.youtube.com/iframe_api";
-				var firstScriptTag = document.getElementByTagName('script')[0];
-				firstScriptTag.parentNade.insertBefore(tag, firstScriptTag);
 
-				var player;
-
-				$window.onYoutubeIframeAPIReady = function(){
-					player = new YT.Player(element.children()[0], {
-						height: scope.height,
-						width: scope.width,
-						videoId: scope.videoId
-					});
-				};
-			},
-		}
-	});
 
 
 

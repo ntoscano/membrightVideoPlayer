bd1b9fcf93938eb935ea7a97b15a5e7133269440
ntoscano <Nicolas.Toscano@gmail.com>
2015-03-23 13:47:41 -0700

    added YT glyph, fixed image display on cards

--- app/deck.js
+++ app/deck.js
@@ -4,7 +4,7 @@
 	app.controller('DeckController',['$location', '$scope','$sce','deckGetter', function($location, $scope, $sce, deckGetter){
 
 		var deckStuff = $location.search();
-
+		console.log(deckStuff.deck);
 		var deckId = parseInt(deckStuff.deck);
 
 		var deckInfo = deckGetter.getNextDeck2(deckId);
@@ -19,16 +19,29 @@
 			};
 			deckInfo.cards.then(function(cards){
 				$scope.deck.cards = cards.objects;
+				console.log($scope.deck.cards);
 				for (var x in $scope.deck.cards){
     	    		time = $scope.deck.cards[x].obj.links[0].label.split(":");
         		    time = [parseInt(time[0]), parseInt(time[1])];
     	    	    $scope.deck.cards[x].time = time[0] * 60 + time[1];
 	            	$scope.deck.cards[x].minTime = $scope.deck.cards[x].obj.links[0].label;
+					
 	        	};
 				if(!$scope.$$phase) $scope.$apply();
 			})
 			$scope.des = $scope.deck.content.description;
 			if(!$scope.$$phase) $scope.$apply();
+			for(var x in $scope.deck.cards){
+				if($scope.deck.cards[x].answer.indexOf("<img", 0) == -1){}
+				else{
+					var i = $scope.deck.cards[x].answer.indexOf("<img", 0);
+					var slice1 = $scope.deck.cards[x].answer.slice(0, i);
+					var o = i + 4;
+					var slice2 = $scope.deck.cards[x].answer.slice(o);
+						$scope.deck.cards[x].answer = slice1 + '<img class="picture"' + slice2;
+				}
+			}
+			console.log($scope.deck.cards);
 		});
 
 
